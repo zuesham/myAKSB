4d3e6933d4c5598ab219b648e2eb1e1f59680060
itsmebays <42906445+zuesham@users.noreply.github.com>
2024-06-05 15:57:24 +0800

    Add files via upload

---
+++ layouts/MainLayout.vue
@@ -0,0 +1,46 @@
+<template>
+  <q-layout view="hHh lPr fFf">
+    <v-header />
+
+    <q-page-container :style="$store.getters.getPageContainerStyle">
+      <router-view  class="q-pb-lg"/>
+    </q-page-container>
+
+    <v-footer />
+  </q-layout>
+</template>
+
+<script>
+import VHeader from 'components/Header'
+import VFooter from 'components/Footer'
+import VDrawer from 'components/drawer'
+
+export default {
+  name: 'MyLayout',
+  components: { VHeader, VFooter, VDrawer },
+  data () {
+    return {
+      modal: false,
+      leftDrawerOpen: this.$q.platform.is.desktop,
+    }
+  }
+}
+</script>
+
+<style scoped>
+  #logo-wrapper img {
+    width: 100%;
+  }
+  #logo-wrapper span {
+    font-size: 1.5em;
+    font-weight: bold;
+  }
+  .text-caption {
+    color: #f7f7f7!important
+  }
+  .q-item.q-router-link--active, .q-item--active {
+    color: #ffffff !important;
+    font-weight: bold;
+    background: var(--q-color-primary);
+  }
+</style>
