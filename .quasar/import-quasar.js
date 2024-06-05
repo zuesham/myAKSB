0bc42cb4932bf9b58815d41a19c494bfe99244b7
itsmebays <42906445+zuesham@users.noreply.github.com>
2024-06-05 15:12:02 +0800

    Create import-quasar.js

---
+++ .quasar/import-quasar.js
@@ -0,0 +1,14 @@
+/**
+ * THIS FILE IS GENERATED AUTOMATICALLY.
+ * DO NOT EDIT.
+ *
+ * You are probably looking on adding startup/initialization code.
+ * Use "quasar new boot <name>" and add it there.
+ * One boot file per concern. Then reference the file(s) in quasar.conf.js > boot:
+ * boot: ['file', ...] // do not add ".js" extension to it.
+ *
+ * Boot files are your "main.js"
+ **/
+import Vue from 'vue'
+import {Quasar,QLayout,QHeader,QFooter,QDrawer,QPageContainer,QPage,QToolbar,QToolbarTitle,QBtn,QIcon,QList,QItem,QItemSection,QItemLabel,QSelect,QRadio,QImg,QSeparator,QSpinnerOval,QPullToRefresh,QInput,QToggle,QCard,QCardSection,QCardActions,QDialog,QSpace,QMenu,QBadge,QAvatar,QChip,QExpansionItem,QTabs,QTab,QRouteTab,QInfiniteScroll,QSpinnerDots,QTable,Ripple,ClosePopup,GoBack,Notify,AddressbarColor,Loading,BottomSheet,Dialog,AppFullscreen} from 'quasar'
+Vue.use(Quasar, { config: {"cordova":{"backButtonExit":true}},components: {QLayout,QHeader,QFooter,QDrawer,QPageContainer,QPage,QToolbar,QToolbarTitle,QBtn,QIcon,QList,QItem,QItemSection,QItemLabel,QSelect,QRadio,QImg,QSeparator,QSpinnerOval,QPullToRefresh,QInput,QToggle,QCard,QCardSection,QCardActions,QDialog,QSpace,QMenu,QBadge,QAvatar,QChip,QExpansionItem,QTabs,QTab,QRouteTab,QInfiniteScroll,QSpinnerDots,QTable},directives: {Ripple,ClosePopup,GoBack},plugins: {Notify,AddressbarColor,Loading,BottomSheet,Dialog,AppFullscreen} })
